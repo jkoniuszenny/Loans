a7db271c2dc6e002f6f0812ef53ba9470cfe773c
Jakub Koniuszenny <Jakub.Koniuszenny@rossmann.com.pl>
2022-08-29 14:55:57 +0200

    Task 1

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,29 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    public class LoanTermShould
+    {
+
+        [Test]
+        public void ToMonthsGet12Test()
+        {
+            LoanTerm loanTerm = new LoanTerm(1);
+
+            var result = loanTerm.ToMonths();
+
+            Assert.That(result, Is.EqualTo(12));
+        }
+
+        [Test]
+        public void ToMonthsExceptionTest()
+        {
+
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
