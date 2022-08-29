49d55462be7eec025f43ca5dff85ba8f95219144
Łukasz Bińkowski <Lukasz.Binkowski@rossmann.com.pl>
2022-08-29 15:20:33 +0200

    3-5

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -32,5 +32,44 @@ namespace Loans.Tests
             LoanTerm loanTerm = new LoanTerm(1);
             Assert.That(loanTerm.Years, Is.EqualTo(1));
         }
+
+
+        [TestCase(5,5)]
+       
+        public void IsYearsEqualToOneTestByYear(int a, int b)
+        {
+            LoanTerm loanTerm = new LoanTerm(a);
+            LoanTerm loanTerm2 = new LoanTerm(b);
+
+            Assert.That(loanTerm, Is.EqualTo(loanTerm2));
+        }
+
+        [TestCase(5, 5)]
+        [TestCase(4, 5)]
+        public void IsYearsEqualToOneTestByInstance(int a, int b)
+        {
+            LoanTerm loanTerm = new LoanTerm(a);
+            LoanTerm loanTerm2 = new LoanTerm(b);
+
+           // Assert.That(loanTerm, Is.Not.SameAs(loanTerm2));
+            LoanTerm loanTerm3 = loanTerm;
+            Assert.That(loanTerm, Is.SameAs(loanTerm3));
+
+        }
+
+        [Test]
+        public void IsStringEqualToOneAnother()
+        {
+
+            List<string> a = new List<string>();
+            List<string> b = new List<string>();
+
+
+          //  Assert.That(a, Is.SameAs(b));
+            List<string> c = a;
+
+            Assert.That(a, Is.SameAs(c));
+
+        }
     }
 }
